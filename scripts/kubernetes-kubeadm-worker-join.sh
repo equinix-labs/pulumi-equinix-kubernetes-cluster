0cf9d15c8dda981f9c8e0f9f2e21e1073052b903
David McKay <david@rawkode.com>
2021-07-08 23:06:44 +0100

    fancy pulumi

--- scripts/kubernetes-kubeadm-worker-join.sh
+++ scripts/kubernetes-kubeadm-worker-join.sh
@@ -2,4 +2,4 @@
 CONTROL_PLANE_IP=$(jq -r ".controlPlaneIp" /tmp/customdata.json)
 JOIN_TOKEN=$(jq -r ".joinToken" /tmp/customdata.json)
 
-kubeadm join --token $JOIN_TOKEN $CONTROL_PLANE_IP:6443
+kubeadm join --token $JOIN_TOKEN --discovery-token-unsafe-skip-ca-verification $CONTROL_PLANE_IP:6443
