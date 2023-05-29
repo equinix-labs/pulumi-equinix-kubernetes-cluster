27464ad2a78eacd202494a54779d1b3091fac01f
ocobleseqx <oscar.cobles@eu.equinix.com>
2023-05-29 21:16:04 +0200

    fix cni configuration, same subnet thatn ClusterConfiguration, update last release with required new args
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

--- cloud-init/scripts/cni-cilium.sh
+++ cloud-init/scripts/cni-cilium.sh
@@ -8,19 +8,25 @@ CONTROL_PLANE_IP=$(jq -r ".controlPlaneIp" /run/customdata.json)
 helm repo add cilium https://helm.cilium.io/
 
 helm template cilium/cilium  \
-		--version 1.10.2 \
+		--version 1.13.3 \
 		--namespace kube-system \
 		--set image.repository=quay.io/cilium/cilium \
+		--set MTU=1500 \
 		--set global.ipam.mode=cluster-pool \
-		--set global.ipam.operator.clusterPoolIPv4PodCIDR=192.168.0.0/16 \
+		--set global.ipam.operator.clusterPoolIPv4PodCIDRList=10.244.0.0/16 \
 		--set global.ipam.operator.clusterPoolIPv4MaskSize=23 \
-		--set global.nativeRoutingCIDR=192.168.0.0/16 \
+		--set global.nativeRoutingCIDR=10.244.0.0/16 \
 		--set global.endpointRoutes.enabled=true \
 		--set global.hubble.relay.enabled=true \
 		--set global.hubble.enabled=true \
 		--set global.hubble.listenAddress=":4244" \
 		--set global.hubble.ui.enabled=true \
-    --set kubeProxyReplacement=probe \
+    --set kubeProxyReplacement=partial \
+    --set nodePort.enabled=true \
+    --set nodePort.enableHealthCheck=false \
+    --set hostServices.enabled=true \
+    --set externalIPs.enabled=true \
+    --set hostPort.enabled=true \
     --set k8sServiceHost=${CONTROL_PLANE_IP} \
     --set k8sServicePort=6443 \
 		> /etc/kubernetes/cilium.yaml
